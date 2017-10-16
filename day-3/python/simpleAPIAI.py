0b349a550a162f4f12febb873ae75c8f09251890
kPatch <irvsteve@gmail.com>
2017-10-16 12:35:41 -0400

    Fix spacing/

--- day-3/python/simpleAPIAI.py
+++ day-3/python/simpleAPIAI.py
@@ -2,7 +2,7 @@ import apiai
 
 # https://dialogflow.com/docs/reference/agent/
 client_token = '<YOUR CLIENT ACCESS TOKEN>'
-ai      = apiai.ApiAI(client_token)
+ai = apiai.ApiAI(client_token)
 request = ai.text_request()
 request.session_id = 'api_ai', '<SESSION ID, UNIQUE FOR EACH USER>'
 request.query = "Where do you live?"
--- day-3/python/simpleGTTS.py
+++ day-3/python/simpleGTTS.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from gtts import gTTS
+
 hello = 'buongiorno'.encode('utf-8')
 tts = gTTS(text= hello, lang='it', slow=False)
 tts.save('hello.mp3')
